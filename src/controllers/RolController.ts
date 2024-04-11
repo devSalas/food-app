7ddd0a7a6e7dd4d6a5eec20d2cfb102d28f61b45
estebans salas sulca <esalassulca@gmail.com>
2024-04-11 16:29:22 -0500

    fix: solucionar conflictos en los controladores y rutas

--- src/controllers/RolController.ts
+++ src/controllers/RolController.ts
@@ -1,41 +1,17 @@
-<<<<<<< HEAD
-import type { Request, Response } from "express";
-import * as services from "../services/RolService";
-=======
 import type { Request, Response } from "express"
 import  * as services from '../services/RolService'
 import { sendJsonResponse } from "../utils/responseHttp"
 import { catchedAsync } from "../utils/catchedAsync"
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 
 export async function getRoles(req: Request, res: Response) {
-	const roles = await services.getRoles();
 
-<<<<<<< HEAD
-	return res.status(200).json({ message: "gerson junior", data: roles });
-=======
     const roles  = await services.getRoles()
     sendJsonResponse(res,201,roles,"roles found")
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 }
 
 export async function createRol(req: Request, res: Response) {
-	const { name } = req.body;
 
-<<<<<<< HEAD
-	try {
-		const roles = await services.createRol({ name });
-		res.status(201).json({ message: "gerson junior created" });
-	} catch (error) {
-		res.status(400).json({ message: "msg" });
-	}
-}
 
-export async function findRolbyName(req: Request, res: Response) {
-	const { name } = req.params;
-	try {
-		const user = await services.getRolbyName({ name });
-=======
         const {name}=req.body
         
         const rol  = await services.createRol({name})
@@ -50,31 +26,12 @@ export async function findRolbyName(req: Request, res: Response) {
     const rol = await services.getRolbyName({name})
     sendJsonResponse(res,200,rol,"rol found")
  
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 
-		res.status(200).json({ message: "gerson junior", data: user });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
+	res.status(200).json({ message: "gerson junior", data: rol });
+
 }
 
 export async function deleteRol(req: Request, res: Response) {
-	const { id } = req.body;
-	try {
-		const user = await services.deleteRol({ id });
-
-<<<<<<< HEAD
-		res.status(204).json({ message: "gerson junior delete" });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
-}
-export async function updateRol(req: Request, res: Response) {
-	const { id } = req.body;
-	const { name } = req.body;
-	try {
-		const user = await services.updateRol({ id, name });
-=======
       
     const {id}=req.body
 
@@ -83,6 +40,7 @@ export async function updateRol(req: Request, res: Response) {
     sendJsonResponse(res,204,rol,"rol deleted")
 
 }
+
 export async function updateRol(req: Request, res: Response) {
     
     const {id}=req.body
@@ -91,16 +49,7 @@ export async function updateRol(req: Request, res: Response) {
     const rol  = await services.updateRol({id,name})
     
     sendJsonResponse(res,200,rol,"rol updated")
-
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
-
-		res.status(200).json({ message: "gerson junior update" });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
 }
-<<<<<<< HEAD
-=======
 
 export const RolController = {
     getRoles: catchedAsync(getRoles),
@@ -112,4 +61,3 @@ export const RolController = {
 
 
 
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,57 +1,18 @@
-<<<<<<< HEAD
-import { Request, Response } from "express";
-
-import * as services from "../services/UserService";
-=======
 import  type { Request, Response } from "express"
 
 import  * as services from '../services/UserService'
 import { sendJsonResponse } from "../utils/responseHttp"
 import { catchedAsync } from "../utils/catchedAsync"
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 
 export async function getUsers(req: Request, res: Response) {
-	try {
-		const users = await services.getUsers();
 
-<<<<<<< HEAD
-		return res.status(200).json({ message: "gerson junior", data: users });
-	} catch (error) {}
-=======
         const users  = await services.getUsers()
         sendJsonResponse(res,200,users,"all users")
 
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 }
 
 export async function createUser(req: Request, res: Response) {
-	const { address, email, image, name, password } = req.body;
 
-<<<<<<< HEAD
-	try {
-		const users = await services.createUser({
-			address,
-			email,
-			image: "",
-			name,
-			password,
-		});
-		res.status(201).json({ message: "gerson junior created" });
-	} catch (error) {
-		res.status(400).json({ message: "msg" });
-	}
-}
-
-export async function findUserbyId(req: Request, res: Response) {
-	const id = 2;
-	try {
-		const user = await services.getUserbyId({ id });
-
-		res.status(200).json({ message: "gerson junior", data: user });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
-=======
     const {address,email,image,name,password}=req.body
 
  
@@ -67,67 +28,27 @@ export async function findUserbyId(req: Request, res: Response) {
     const user = await services.getUserbyId({id})
     sendJsonResponse(res,201,user,"user found")
 
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 }
 export async function findUserbyName(req: Request, res: Response) {
 	const { name } = req.params;
 
-<<<<<<< HEAD
-	try {
-		const user = await services.getUserbyName({ name });
-
-		res.status(200).json({ message: "gerson junior", data: user });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
-=======
     const user = await services.getUserbyName({name})
 
     res.status(200).json({ message: "gerson junior",data:user })
 
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 }
 
 export async function deleteUser(req: Request, res: Response) {
 	const id = 2;
-
-	try {
-		const user = await services.deleteUser({ id });
-
-<<<<<<< HEAD
-		res.status(204).json({ message: "gerson junior delete" });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
-=======
-    
     const user = await services.deleteUser({id}) 
 
     sendJsonResponse(res,204,user,"user deleted")
 
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62
 }
 export async function updateUser(req: Request, res: Response) {
 	const id = 2;
 	const { address, email, image, name, password } = req.body;
 
-<<<<<<< HEAD
-	try {
-		const user = await services.updateUser({
-			id,
-			address,
-			email,
-			image,
-			name,
-			password,
-		});
-
-		res.status(200).json({ message: "gerson junior update" });
-	} catch (error) {
-		res.status(404).json({ message: "msg" });
-	}
-}
-=======
 
     const user  = await services.updateUser({id,address,email,image,name,password})
     
@@ -143,4 +64,3 @@ export const UserController = {
     updateUser:catchedAsync(updateUser),
     deleteUser:catchedAsync(deleteUser)
 }
->>>>>>> 9f5301165a0298de077c117ad80751ea17112c62

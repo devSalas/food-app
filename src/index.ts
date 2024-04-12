8555296a8f864f6252d037e08acffa356ce11ce3
Jose <dstryr77@gmail.com>
2024-04-12 12:02:24 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
 import { errorHandler } from "./utils/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler);

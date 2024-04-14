027d26edf2c90d6ec52a31197b8484cbb32a2add
Gerson Garayar <garayargerson20@gmail.com>
2024-04-14 17:49:31 -0500

    agregando schema de zod

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -2,26 +2,18 @@ import z from "zod";
 
 const productSchema = z.object({
   name: z.string(),
-  brand: z.string(),
-  description: z.string(),
   price: z.number(),
-  discount: z.number().min(0).max(100).default(0),
-  images: z.array(z.string()),
-  category: z.string(),
-  sizes: z.array(z.number()),
-  colors: z.array(z.string()),
-  gender: z.string(),
-  isNewArrival: z.boolean().default(false),
-  isBestSeller: z.boolean().default(false),
+  image: z.string(),
+  category_id: z.number(),
 });
 
-export type Shoe = z.infer<typeof productSchema>;
+export type Product = z.infer<typeof productSchema>;
 
-export const validateShoe = (object: any) => {
+export const validateProduct = (object: any) => {
   return productSchema.safeParse(object);
 };
 
-export const validateShoePartial = (object: any) => {
+export const validateProductPartial = (object: any) => {
   const shoeSchemaPartial = productSchema.partial();
   return shoeSchemaPartial.safeParse(object);
 };

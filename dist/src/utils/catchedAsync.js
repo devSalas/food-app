26d20d1d6278c07daeb887edc1e2a4550a7a4fac
Jose <dstryr77@gmail.com>
2024-04-15 10:05:22 -0500

    no track dist/

--- dist/src/utils/catchedAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchedAsync = void 0;
-const catchedAsync = (fn) => {
-	return (req, res, next) => {
-		fn(req, res).catch((err) => next(err));
-	};
-};
-exports.catchedAsync = catchedAsync;
